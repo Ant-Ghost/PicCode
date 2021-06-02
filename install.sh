fb55d73ac30875e668a014b9cebbcd11d335ff32
Ant-Ghost <aunomitra.ghosh1999@gmail.com>
2021-06-02 07:20:32 +0000

    install.sh working properly

--- install.sh
+++ install.sh
@@ -32,51 +32,15 @@ clear
 
 python3 PicCode.py
 
+read -n 1 -s -r -p "Press any key to continue"
+
 clear
 
-read -n 1 -s -r -p "Installation done .... Press any key to continue"
-
 exit 0
 EOF
 
-cat > uninstall.sh << 'EOF'
-#!/bin/bash
+cp additions/uninstall_linux.txt uninstall.sh
 
-echo -e "\n\n***WARNING***\n"
-echo "Do you want to uninstall Pillow?(Y/N)"
-read -n 1
-
-if [[ $REPLY == 'Y' ]] || [[ $REPLY -eq 'y' ]]
-then
-        echo -e "Running command: python3 -m pip -y uninstall Pillow"
-        echo y | python3 -m pip uninstall Pillow
-if
-
-echo -e "\n\n***WARNING***\n"
-read -n 1 -s -r -p "Do you want to uninstall python3-pip?(Y/N)" $CH
-
-if [ $CH -eq 'Y' -o $CH -eq 'y' ]
-then
-        echo -e "Running command: sudo apt-get uninstall python3-pip"
-        sudo apt-get uninstall python3-pip
-fi
-
-echo -e "\n\n***WARNING***\n"
-read -n 1 -s -r -p "Do you want to uninstall python3?(Y/N)" $CH
-
-if [ $CH -eq 'Y' -o $CH -eq 'y' ]
-then
-        echo -e "Running command: sudo apt-get uninstall python3"
-        sudo apt-get uninstall python3
-fi
-
-echo -e "\n\n"
-echo -e "Uninstallation complete"
-
-rm run.sh uninstall.sh
-
-exit 0
-EOF
 chmod 777 run.sh uninstall.sh
 
 read -n 1 -s -r -p "Installation done .... Press any key to continue"
